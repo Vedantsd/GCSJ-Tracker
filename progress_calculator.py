b7e2075ea8e4f90fcd20d80891f516b867eaae81
Vedant Desai <87595836+Vedantsd@users.noreply.github.com>
2025-10-27 10:13:22 +0530

    Update progress_calculator.py

--- progress_calculator.py
+++ progress_calculator.py
@@ -7,34 +7,38 @@ def progress(badges):
         "Get Started with Looker",
         "Get Started with Dataplex",
         "Get Started with Google Workspace Tools",
-        "App Building with Appsheet",
+        "App Building with AppSheet",
         "Develop with Apps Script and AppSheet",
         "Build a Website on Google Cloud",
         "Set Up a Google Cloud Network",
         "Store, Process, and Manage Data on Google Cloud - Console",
-        "Cloud Functions: 3 Ways",
+        "Cloud Run Functions: 3 Ways",
         "App Engine: 3 Ways",
         "Cloud Speech API: 3 Ways",
         "Monitoring in Google Cloud",
         "Analyze Speech and Language with Google APIs",
         "Prompt Design in Vertex AI",
-        "Develop GenAI Apps with Gemini and Streamlit"
+        "Develop Gen AI Apps with Gemini and Streamlit"
     ]
 
     completed = 0
     arcade_game = 0
+    completed_badges = []
+    completed_games = []
 
     for badge in badges: 
         if badge in skill_badge_list:
+            completed_badges.append(badge)
             completed += 1
 
         if "Level 3" in badge:
             arcade_game += 1
+            completed_games.append(badge)
     
-    percent = ((completed + arcade_game) / 20) * 100
+    percent = int(((completed + arcade_game) / 20) * 100)
     if (percent == 100) :
         progress = "Completed"
     else: 
         progress = "Pending"
 
-    return completed, arcade_game, progress, percent
+    return completed, arcade_game, progress, percent, completed_badges, completed_games
